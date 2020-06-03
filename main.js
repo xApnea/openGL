a3c37cf3bf82616e480bc66df2fccf05e7d95481
xApnea <jcbruno150@gmail.com>
2020-06-02 23:31:36 -0400

    A Black Box

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+function main() {
+  const canvas = document.querySelector("#glCanvas");
+
+  //Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  if (gl === null) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+  // Set clear color to black
+  //             R    G    B    OPACITY
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // clear the color buffer with above color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
